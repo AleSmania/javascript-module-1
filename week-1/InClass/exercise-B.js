ed49ad47661afcc452e656edcfc6c7d4ca20515a
AleSmania <97732603+AleSmania@users.noreply.github.com>
2022-02-23 19:24:01 +0100

    saludos

---
+++ week-1/InClass/exercise-B.js
@@ -0,0 +1,11 @@
+console.log ("hello world")
+console.log ("Halo, dunia!")
+console.log ("Ciao, mondo!")
+console.log("Hola, mundo!")
+console.log("привет")
+console.log("Aloha")
+console.log("Merhaba")
+console.log("Konnichi wa")
+console.log("Olá")
+console.log("Kaixo")
+
