2e428b81cf0f82f1ebe77ebfcc942a04b3231f7e
AleSmania <97732603+AleSmania@users.noreply.github.com>
2022-02-26 13:44:42 +0100

    result

--- week-1/Homework/K-functions-parameters/exercise4.js
+++ week-1/Homework/K-functions-parameters/exercise4.js
@@ -1,10 +1,8 @@
 // Declare your function first
-function sum( number1,  number2)  { 
-    return  number1 +=  number2 ; 
-  }
+function numbers(num1, num2){
+    return num1 + num2;
+}
 // Call the function and assign to a variable `sum`
-var result=sum(13,124);
-
-
-console.log(sum);
+const sum = numbers(13, 124);
+console.log(sum); // prints 137
 
