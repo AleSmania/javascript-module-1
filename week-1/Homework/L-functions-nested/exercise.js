9e0d6759976b0407838b22b83e2f1fa361f136c4
AleSmania <97732603+AleSmania@users.noreply.github.com>
2022-02-26 14:05:49 +0100

    no resultado

--- week-1/Homework/L-functions-nested/exercise.js
+++ week-1/Homework/L-functions-nested/exercise.js
@@ -4,3 +4,11 @@ var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
 
+function createGreeting( mentor1,){
+    return `Hello, my name is ${mentor1}`;
+}
+const greeting = createGreeting("Daniel");
+function gritar (mentor1) {
+    return "¡" + toUpperCase(mentor1) + "!"
+}
+console.log(greeting);     
