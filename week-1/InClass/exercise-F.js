ea7da32c13cda8dd744a565d700d07dc87a44006
AleSmania <97732603+AleSmania@users.noreply.github.com>
2022-03-02 20:27:58 +0100

    suma

--- week-1/InClass/exercise-F.js
+++ week-1/InClass/exercise-F.js
@@ -1,2 +1,7 @@
 const numberOfStudents=15
 const numberOfMentors=8
+
+function suma (numberOfMentors, numberOfStudents){
+    return numberOfMentors + numberOfStudents
+}
+console.log (suma (15, 8));
